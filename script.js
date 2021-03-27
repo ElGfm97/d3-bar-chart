ff594bf21a94da3516e50489aa0d6a78721a8312
ElGfm <el.gfm97@gmail.com>
2021-03-27 17:20:06 +0100

    Added x-axis to bar chart

--- script.js
+++ script.js
@@ -1,62 +1,40 @@
 let dataset = [];
-const w = 800;
-const h = 400;
-const padding = 30;
+let width = 800,
+    height = 400;
 
 fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json').then(
     response => response.json()).then(
     data => {
-        for(let i = 0; i < data.data.length; i++) {
+        for (let i = 0; i < data.data.length; i++) {
             dataset.push(data.data[i]);
         }
-        /*
-        const scale = d3.scaleLinear();
-        const minX = d3.min(dataset, (d) => d[1]);
-        const maxX = d3.max(dataset, (d) => d[1]);
-        const minY = dataset[0][0];
-        const maxY = dataset[dataset.length - 1][0];
 
-        const xScale = d3.scaleLinear().domain([0, maxX]).range([padding, w - padding]);
-        const yScale = d3.scaleLinear().domain([0, maxY]).range([h - padding, padding]);
+        let barWidth = width / dataset.length;
 
-
-        d3.select('#chart-container')
-            .selectAll('div')
-            .data(dataset)
-            .enter()
-            .append('div')
-            .attr("class", "bar")
-            .style("height", (d) => d[1] + "px");
-        const svg = d3.select('#chart-container')
+        const svg = d3
+            .select('#chart')
             .append('svg')
-            .attr("width", w)
-            .attr("height", h);
+            .attr('width', width + 100)
+            .attr('height', height + 60);
 
-        svg.selectAll("rect")
-            // Add your code below this line
-            .data(dataset)
-            .enter()
-            .append("rect")
-            .attr("x", (d, i) => xScale(i * 200))
-            .attr("y", (d, i) => yScale(h - d[1]))
-            .attr("width", 3)
-            .attr("height", (d, i) => (d[1]))
-            .attr("fill", "navy")
-            .append("title")
-            .text((d) => d[0]);
 
-        svg.selectAll("text")
-            .data(dataset)
-            .enter()
-            .append("text")
-            .attr("x", (d, i) => xScale(i * 40))
-            .attr("y", (d, i) => yScale(h - d[1] - 6))
-            .text((d, i) => d[1]); */
+        const dates = dataset.map(function (item) {
+            return new Date(item[0]);
+        });
 
-          const svg = d3.select('#chart')
-              .append('svg')
-              .attr('width', w)
-              .attr('height', h);
+        const xMax = new Date(d3.max(dates));
+        const xMin = new Date(d3.min(dates));
+        // xMax.setMonth(xMax.getMonth() + 3);
+        const xScale = d3
+            .scaleTime()
+            .domain([xMin, xMax])
+            .range([0, width]);
 
+        const xAxis = d3.axisBottom().scale(xScale);
+
+        svg.append('g')
+            .call(xAxis)
+            .attr('id', 'x-axis')
+            .attr('transform', 'translate(30, 400)');
 
     });
